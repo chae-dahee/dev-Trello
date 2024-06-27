5f25b12dfd0729df6ce298d3f1aaa413d4dce697
dah <cdh010126r@gmail.com>
2024-06-27 15:47:56 +0900

    feat. BoardList 상태

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { appContainer, board, buttons } from "./App.css";
+import BoardList from "./components/BoardList/BoardList";
+
+function App() {
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
+  return (
+    <div className={appContainer}>
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
+      <div className={board}></div>
+      <div>
+        <button className={buttons}>이 게시판 삭제하기</button>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
