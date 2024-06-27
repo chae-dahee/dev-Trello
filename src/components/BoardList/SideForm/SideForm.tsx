14620d9ed6cddcfeb98a316b27ee3bb72e256d1e
dah <cdh010126r@gmail.com>
2024-06-27 15:53:12 +0900

    feat. 새게시물 입력, 등록

---
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -0,0 +1,62 @@
+import React, { ChangeEvent, FC, useState } from "react";
+import { FiCheck } from "react-icons/fi";
+import { icon, in put, sideFrom } from "./SideForm.css";
+import { useTypedDispatch } from "../../../hooks/redux";
+import { v4 as uuidv4 } from "uuid";
+import { addBoard } from "../../../store/slices/boardsSlice";
+import { addLog } from "../../../store/slices/loggerSlice";
+
+type TSideFromProps = {
+  inputRef: React.RefObject<HTMLInputElement>;
+  setIsFromOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SideForm: FC<TSideFromProps> = ({ setIsFromOpen, inputRef }) => {
+  const [inputText, setinputText] = useState("");
+  const dispatch = useTypedDispatch();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setinputText(e.target.value);
+  };
+
+  const handleOnBlur = () => {
+    setIsFromOpen(false);
+  };
+
+  const handleClick = () => {
+    if (inputText) {
+      dispatch(
+        addBoard({
+          board: { boardId: uuidv4(), boardName: inputText, lists: [] },
+        })
+      );
+
+      dispatch(
+        addLog({
+          logId: uuidv4(),
+          logMessage: `게시판 등록 : ${inputText}`,
+          logAuthor: "User",
+          logTimestamp: String(Date.now()),
+        })
+      );
+    }
+  };
+
+  return (
+    <div className={sideFrom}>
+      <input
+        // ref={inputRef}
+        autoFocus
+        className={input}
+        type="text"
+        placeholder="새로운 게시판 등록하기"
+        value={inputText}
+        onChange={handleChange}
+        onBlur={handleOnBlur}
+      />
+      <FiCheck className={icon} onMouseDown={handleClick} />
+    </div>
+  );
+};
+
+export default SideForm;
