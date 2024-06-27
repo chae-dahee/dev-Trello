80b3b436744bd8db9f731adbafebf194035337e2
dah <cdh010126r@gmail.com>
2024-06-27 15:52:47 +0900

    feat. BoardList id 구분, 활성화 및 이동

---
+++ src/components/BoardList/BoardList.tsx
@@ -0,0 +1,69 @@
+import React, { FC, useRef, useState } from "react";
+import { useTypedSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiPlusCircle } from "react-icons/fi";
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from "./BoardList.css";
+import clsx from "clsx";
+
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const { boardArray } = useTypedSelector((state) => state.boards); //hooks로 만든 데이터
+  const [isFromOpen, setIsFromOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    setIsFromOpen(!isFromOpen);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  return (
+    <div className={container}>
+      <div className={title}>게시판 :</div>
+      {boardArray.map((board, index) => (
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)}
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) ===
+                index,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                index,
+            }
+          )}
+        >
+          <div>{board.boardName}</div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {isFromOpen ? (
+          <SideForm inputRef={inputRef} setIsFromOpen={setIsFromOpen} />
+        ) : (
+          <FiPlusCircle className={addButton} onClick={handleClick} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BoardList;
