656b963bdbbf05b2c5f0a7490de67393d47a1aff
dah <cdh010126r@gmail.com>
2024-06-28 17:18:32 +0900

    fix. 오타

--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, FC, useState } from "react";
 import { FiCheck } from "react-icons/fi";
 import { icon, input, sideFrom } from "./SideForm.css";
 import { useTypedDispatch } from "../../../hooks/redux";
-import { v4 as uuidv4 } from "uuid";
+import { v4 } from "uuid";
 import { addBoard } from "../../../store/slices/boardsSlice";
 import { addLog } from "../../../store/slices/loggerSlice";
 
@@ -27,13 +27,13 @@ const SideForm: FC<TSideFromProps> = ({ setIsFromOpen, inputRef }) => {
     if (inputText) {
       dispatch(
         addBoard({
-          board: { boardId: uuidv4(), boardName: inputText, lists: [] },
+          board: { boardId: v4(), boardName: inputText, lists: [] },
         })
       );
 
       dispatch(
         addLog({
-          logId: uuidv4(),
+          logId: v4(),
           logMessage: `게시판 등록 : ${inputText}`,
           logAuthor: "User",
           logTimestamp: String(Date.now()),
