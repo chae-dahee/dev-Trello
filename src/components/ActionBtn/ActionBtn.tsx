ed1410318daad9af889dc9d998cfbef88dc9c5a2
dah <cdh010126r@gmail.com>
2024-06-28 14:00:17 +0900

    추가 버튼

--- src/components/ActionBtn/ActionBtn.tsx
+++ src/components/ActionBtn/ActionBtn.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { FC, useState } from "react";
+import DropDownForm from "./DropDownForm/DropDownForm";
+import { IoIosAdd } from "react-icons/io";
+import { listButton, taskButton } from "./ActionBtn.css";
 
-const ActionBtn = () => {
-  return (
-    <div>ActionBtn</div>
-  )
-}
+type TActionBtnProps = {
+  boardId: string;
+  listId: string;
+  list?: boolean;
+};
 
-export default ActionBtn
+const ActionBtn: FC<TActionBtnProps> = ({ boardId, listId, list }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const buttonText = list ? "새로운 리스트 등록" : "새로운 일 등록";
+
+  return isFormOpen ? (
+    <DropDownForm
+      setIsFormOpen={setIsFormOpen}
+      list={list ? true : false}
+      boardId={boardId}
+      listId={listId}
+    />
+  ) : (
+    <div
+      className={list ? listButton : taskButton}
+      onClick={() => setIsFormOpen(true)}
+    >
+      <IoIosAdd></IoIosAdd>
+      <p>{buttonText}</p>
+    </div>
+  );
+};
+
+export default ActionBtn;
