e2e96a642f432942375c115808009dfa47fa3bba
dah <cdh010126r@gmail.com>
2024-06-29 23:54:47 +0900

    firebase 세팅

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEo7u9sSXfUC3WxoLl-2Hb4ZiFimSVMI4",
+  authDomain: "dev-trello.firebaseapp.com",
+  projectId: "dev-trello",
+  storageBucket: "dev-trello.appspot.com",
+  messagingSenderId: "304146701084",
+  appId: "1:304146701084:web:4ce24416215f6d2fa638d9",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
